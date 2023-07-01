75bc371348c880bfb8f5b4c7bf6c0099bcf07697
sundarasekar <sundarasekar01@gmail.com>
2023-07-01 16:35:32 +0530

    changed

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 const express = require('express')
 const app = express()
 
+
+const details =[
+    
+    {
+    name: 'sundara',
+    age:22,
+    place:'pothencode'
+},
+{
+    name: 'karthik',
+    age:21,
+    place:'attingal'
+}
+
+]
+
+
+app.get('/details', (req,res) =>{
+
+    res.json(details)
+})
+
+
 app.listen(4000)
